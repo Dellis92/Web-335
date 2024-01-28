39ebc3b05a2cc5a5f8300178683a1672a9b3b5c0
DeVonte <devonte.ellis@gmail.com>
2024-01-28 15:55:14 -0600

    week 3

---
+++ week-3/ellis_calculator.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    return a+b
+
+
+def subtract(c,d):
+    return c-d
+
+
+def divide(e,f):
+    return e/f
+
+
+def multiply(g,h):
+    return g*h
+
+print(add(1,2))
+print(subtract(5,3))
+print(divide(9,3))
+print(multiply(3,5))
