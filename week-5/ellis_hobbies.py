ffb5f767e8b5869b41b6c623087a07c36bfcbd2e
DeVonte <devonte.ellis@gmail.com>
2024-02-09 17:01:42 -0600

    week 5 python

---
+++ week-5/ellis_hobbies.py
@@ -0,0 +1,12 @@
+hobbies = ["gaming", "movies", "reading", "drawing", "music"]
+
+for hobby in hobbies:
+        print(hobby)
+    
+days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+
+for day in days:
+    if days == ["saturday","sunday"]:
+        print("You are off, enjoy your hobbies")
+else:
+      print("Sorry, but you should be working")
